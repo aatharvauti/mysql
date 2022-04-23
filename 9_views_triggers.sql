0300d4b3debe4c717e85ad155413ba55e8d90f82
aatharvauti <bm.aatharvauti@protonmail.ch>
2022-04-23 21:00:05 +0530

    Add 9/ViewsTriggers

---
+++ 9_views_triggers.sql
@@ -0,0 +1,24 @@
+
+-- Views and Triggers
+CREATE DATABASE triggerdb;
+USE triggerdb;
+
+CREATE TABLE Students(StudentName varchar(45), RollNo int, PRIMARY KEY (user_id));
+
+CREATE TABLE trigger_timestamp (exec_time datetime NOT NULL);
+
+-- INSERT Timestamp whenever a new row or column is inserted in Students
+CREATE TRIGGER user_trig BEFORE INSERT
+    ON Students FOR EACH ROW
+    INSERT INTO trigger_timestamp VALUES(NOW());
+
+INSERT INTO Students VALUES('Atharva Auti', 1);
+INSERT INTO Students VALUES('Daenerys Targaryen', 2);
+INSERT INTO Students VALUES('Jon Snow', 3);
+INSERT INTO Students VALUES('Arya Stark', 4);
+
+-- Shows all TRIGGERS in detail
+SHOW TRIGGERS;
+
+-- VIEW TABLE trigger_timestamp
+SELECT * FROM trigger_timestamp;
